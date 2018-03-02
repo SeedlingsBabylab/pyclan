8ebd855658fcc82bf19dedb5fd9694a0c89e39f4
websterbei <bei.yijie.2014@vjc.sg>
2018-03-02 10:48:42 -0500

    fixed parser error missing __repr__

--- pyclan/errors.py
+++ pyclan/errors.py
@@ -2,3 +2,6 @@ class ParseError(Exception):
     def __init__(self, line_num, last_line):
         self.index = line_num
         self.last_line = last_line
+
+    def __repr__(self):
+    	return "Parser Error:\n\t index: {}\n\tline: {}".format(self.index, self.last_line)
