adf5a649dd617792b91866c2ec2a4cc263660c19
Gladys Baudet <gladys.baudet@gmail.com>
2018-12-28 16:55:57 -0500

    changed + into * to account for single letter words

--- pyclan/elements.py
+++ pyclan/elements.py
@@ -258,9 +258,9 @@ pause_regx = re.compile("Pause (\d+)")
 xdb_regx = re.compile(
     "average_dB=\"([-+]?[0-9]*\.?[0-9]+)\" peak_dB=\"([-+]?[0-9]*\.?[0-9]+)\"")
 code_regx = re.compile(
-    '([a-zA-Z][a-z+]+)( +)(&=)([A-Za-z]{1})(_)([A-Za-z]{1})(_)([A-Z]{1}[A-Z0-9]{2})(_)?([a-z0-9]{6})?', re.IGNORECASE | re.DOTALL)
+    '([a-zA-Z][a-z+]*)( +)(&=)([A-Za-z]{1})(_)([A-Za-z]{1})(_)([A-Z]{1}[A-Z0-9]{2})(_)?([a-z0-9]{6})?', re.IGNORECASE | re.DOTALL)
 code_regx_id = re.compile(
-    '([a-zA-Z][a-z+]+)( +)(&=)([A-Za-z]{1})(_)([A-Za-z]{1})(_)([A-Z]{1}[A-Z0-9]{2})(_)([a-z0-9]{6})', re.IGNORECASE | re.DOTALL)
+    '([a-zA-Z][a-z+]*)( +)(&=)([A-Za-z]{1})(_)([A-Za-z]{1})(_)([A-Z]{1}[A-Z0-9]{2})(_)([a-z0-9]{6})', re.IGNORECASE | re.DOTALL)
 #code_regx = re.compile(
 #    '([a-zA-Z+]+)( +)(&=)(.)(_)(.)(_)([a-zA-Z0-9]{3})', re.IGNORECASE | re.DOTALL)
 # annot_regx = re.compile('(&=)(.)(_)(.)(_)([a-zA-Z0-9]{3})')
