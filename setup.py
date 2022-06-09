afd977e78a63a5e059a0b1a6cb8fa404cd4ba74a
Zhenya <e.kalenkovich@gmail.com>
2022-06-09 09:16:54 -0400

    add requirements to setup.py
    So that they are installed together with the package.

--- setup.py
+++ setup.py
@@ -2,6 +2,17 @@ from setuptools import setup, find_packages
 
 __version__ = '0.2'
 
+# The versions were copied from requirements.txt
+install_requires = [
+      'Distance==0.1.3',
+      'mysql-connector==2.2.9',
+      'numpy',
+      'pandas',
+      'python-dateutil==2.8.1',
+      'pytz==2020.1',
+      'six==1.15.0'
+]
+
 setup(name='pyclan',
       version=__version__,
       description='CLAN file library',
@@ -9,5 +20,6 @@ setup(name='pyclan',
       author_email='andrei.amatuni@gmail.com',
       license="MIT",
       url='https://github.com/SeedlingsBabylab/pyclan',
-      packages=find_packages()
+      packages=find_packages(),
+      install_requires=install_requires
 )
