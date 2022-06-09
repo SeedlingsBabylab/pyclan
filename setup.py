b32e36f1f2b530c405ad316e0636e470ac79683f
Zhenya <e.kalenkovich@gmail.com>
2022-06-09 09:09:45 -0400

    flexible versions of numpy and pandas

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 __version__ = '0.2'
 
-# This is intentionally an almost direct copy of requirements.txt (except for pyclan)
+# The versions were copied from requirements.txt
 install_requires = [
       'Distance==0.1.3',
       'mysql-connector==2.2.9',
-      'numpy==1.19.2',
-      'pandas==1.1.2',
+      'numpy>=1',
+      'pandas>=1',
       'python-dateutil==2.8.1',
       'pytz==2020.1',
       'six==1.15.0'
